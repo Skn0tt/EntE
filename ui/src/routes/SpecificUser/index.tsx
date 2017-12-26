2909da1d3b835718e5cd51cff61e607227f3d9d2
Simon Knott <simoknott@gmail.com>
2017-12-26 21:57:24 +0100

    Add Route for Viewing Specific User

---
+++ ui/src/routes/SpecificUser/index.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import withStyles, { WithStyles } from 'material-ui/styles/withStyles';
+import { connect, Dispatch } from 'react-redux';
+import styles from './styles';
+
+import * as select from '../../redux/selectors';
+import { AppState, MongoId, User } from '../../interfaces/index';
+import { Action } from 'redux';
+
+import { withRouter, RouteComponentProps } from 'react-router';
+import Modal from '../../components/Modal';
+
+interface RouteMatch {
+  userId: MongoId;
+}
+
+interface Props extends WithStyles, RouteComponentProps<RouteMatch> {
+  getUser(id: MongoId): User;
+}
+
+const SpecificUser: React.SFC<Props> = (props) => (
+  <Modal
+    onClose={() => props.history.goBack()}
+  >
+    <div>
+      TEst!!
+    </div>
+  </Modal>
+);
+
+const mapStateToProps = (state: AppState) => ({
+  getUser: (id: MongoId) => select.getUser(id)(state),
+});
+
+const mapDispatchToProps = (dispatch: Dispatch<Action>) => ({});
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(SpecificUser)));
