f4ec0a150b251ec9390aa533e4aac083c0f80f57
Simon Knott <simoknott@gmail.com>
2017-12-26 21:53:27 +0100

    Change Theme

---
+++ ui/src/theme.ts
@@ -0,0 +1,10 @@
+import createMuiTheme from 'material-ui/styles/createMuiTheme';
+import blue from 'material-ui/colors/blue';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+  }
+});
+
+export default theme;
