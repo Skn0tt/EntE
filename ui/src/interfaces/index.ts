3a090a053ec377f843159c23e03401ba7e86e02a
Simon Knott <simoknott@gmail.com>
2018-01-05 20:23:09 +0100

    Remove Credentials

--- ui/src/interfaces/index.ts
+++ ui/src/interfaces/index.ts
@@ -155,8 +155,8 @@ export interface IAuth extends ICredentials {
 }
 
 export class AuthState extends Record({
-  username: 'admin',
-  password: 'root',
+  username: '',
+  password: '',
   displayname: '',
   role: '',
   children: [],
