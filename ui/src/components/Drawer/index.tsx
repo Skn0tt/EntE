dd7a3499cc596075717e4507c643db0342a8a96b
Simon Knott <simoknott@gmail.com>
2018-01-06 14:05:54 +0100

    rework interfaces

--- ui/src/components/Drawer/index.tsx
+++ ui/src/components/Drawer/index.tsx
@@ -22,7 +22,9 @@ import { AppState, Roles } from '../../interfaces/index';
 import { withRouter, RouteComponentProps } from 'react-router';
 import LoginStatus from '../LoginStatus';
 
-interface IProps {
+interface OwnProps {}
+
+interface StateProps {
   loading: boolean;
   role: Roles;
 }
@@ -36,87 +38,89 @@ const mapStateToProps = (state: AppState) => ({
   role: select.getRole(state),
 });
 
-type Props = IProps & RouteComponentProps<{}> & WithStyles<string>;
+type Props = OwnProps & StateProps & RouteComponentProps<{}> & WithStyles<string>;
 
-const Drawer = withRouter(connect(mapStateToProps)(withStyles(styles)(
-  class extends React.Component<Props, State> {
-    state = {
-      mobileOpen: false,
-    };
-  
-    handleDrawerToggle = () => this.setState({ mobileOpen: !this.state.mobileOpen });
-  
-    render() {
-      const classes = this.props.classes!;
-  
-      const drawer = (
-        <div>
-          <LoginStatus />
-          <Divider />
-          <StandardItems />
-          <Divider />
-          {this.props.role === Roles.ADMIN && <AdminItems />}
-          {this.props.role === Roles.TEACHER && <TeacherItems />}
-          {this.props.role === Roles.PARENT && <ParentItems />}
-          {this.props.role === Roles.STUDENT && <StudentItems />}
-        </div>
-      );
-  
-      return (
-        <div className={classes.root}>
-          {this.props.loading && <LoadingIndicator />}
-          <div className={classes.appFrame}>
-            <AppBar className={classes.appBar}>
-              <Toolbar>
-                <IconButton
-                  color="contrast"
-                  aria-label="open drawer"
-                  onClick={this.handleDrawerToggle}
-                  className={classes.navIconHide}
-                >
-                  <MenuIcon />
-                </IconButton>
-                <Typography type="title" color="inherit" noWrap={true}>
-                  EntschuldigungsVerfahren
-                </Typography>
-                <div className={classes.grow} />
-                <RefreshButton />
-              </Toolbar>
-            </AppBar>
-            <Hidden mdUp={true}>
-              <MUIDrawer
-                type="temporary"
-                open={this.state.mobileOpen}
-                classes={{
-                  paper: classes.drawerPaper,
-                }}
-                onClose={this.handleDrawerToggle}
-                ModalProps={{
-                  keepMounted: true,
-                }}
+const Drawer =
+  withRouter(
+  connect<StateProps, null, OwnProps>(mapStateToProps)(
+  withStyles(styles)(
+class extends React.Component<Props, State> {
+  state = {
+    mobileOpen: false,
+  };
+
+  handleDrawerToggle = () => this.setState({ mobileOpen: !this.state.mobileOpen });
+
+  render() {
+    const classes = this.props.classes!;
+
+    const drawer = (
+      <div>
+        <LoginStatus />
+        <Divider />
+        <StandardItems />
+        <Divider />
+        {this.props.role === Roles.ADMIN && <AdminItems />}
+        {this.props.role === Roles.TEACHER && <TeacherItems />}
+        {this.props.role === Roles.PARENT && <ParentItems />}
+        {this.props.role === Roles.STUDENT && <StudentItems />}
+      </div>
+    );
+
+    return (
+      <div className={classes.root}>
+        {this.props.loading && <LoadingIndicator />}
+        <div className={classes.appFrame}>
+          <AppBar className={classes.appBar}>
+            <Toolbar>
+              <IconButton
+                color="contrast"
+                aria-label="open drawer"
+                onClick={this.handleDrawerToggle}
+                className={classes.navIconHide}
               >
-                {drawer}
-              </MUIDrawer>
-            </Hidden>
-            <Hidden mdDown={true} implementation="css">
-              <MUIDrawer
-                type="permanent"
-                open={true}
-                classes={{
-                  paper: classes.drawerPaper,
-                }}
-              >
-                {drawer}
-              </MUIDrawer>
-            </Hidden>
-            <main className={classes.content}>
-              {this.props.children}
-            </main>
-          </div>
+                <MenuIcon />
+              </IconButton>
+              <Typography type="title" color="inherit" noWrap={true}>
+                EntschuldigungsVerfahren
+              </Typography>
+              <div className={classes.grow} />
+              <RefreshButton />
+            </Toolbar>
+          </AppBar>
+          <Hidden mdUp={true}>
+            <MUIDrawer
+              type="temporary"
+              open={this.state.mobileOpen}
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+              onClose={this.handleDrawerToggle}
+              ModalProps={{
+                keepMounted: true,
+              }}
+            >
+              {drawer}
+            </MUIDrawer>
+          </Hidden>
+          <Hidden mdDown={true} implementation="css">
+            <MUIDrawer
+              type="permanent"
+              open={true}
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+            >
+              {drawer}
+            </MUIDrawer>
+          </Hidden>
+          <main className={classes.content}>
+            {this.props.children}
+          </main>
         </div>
-      );
-    }
-  },
-)));
+      </div>
+    );
+  }
+})));
 
 export default Drawer;
