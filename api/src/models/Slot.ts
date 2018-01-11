13f563e0346000c83d25c52224a7cf3761b010fd
Simon Knott <simoknott@gmail.com>
2018-01-11 20:00:16 +0100

    Works

--- api/src/models/Slot.ts
+++ api/src/models/Slot.ts
@@ -3,7 +3,9 @@ import { MongoId } from '../constants';
 import * as validate from 'mongoose-validator';
 import * as idValidator from 'mongoose-id-validator';
 
-export interface SlotModel extends Document, ISlot {}
+export interface SlotModel extends Document, ISlot {
+  sign(): void;
+}
 
 export interface ISlot {
   date: Date;
@@ -20,12 +22,21 @@ const slotSchema = new Schema({
   date: { type: Date, required: true, default: Date.now() },
   hour_from: { type: Number, required: true, min: 1, max: 12 },
   hour_to: { type: Number, required: true, min: 1, max: 12 },
+  signed: { type: Boolean, required: true, default: false },
   student: { type: Schema.Types.ObjectId, required: true, ref: 'users' },
   teacher: { type: Schema.Types.ObjectId, required: true, ref: 'users' },
 }, {
   versionKey: false,
 });
 slotSchema.plugin(idValidator);
+
+/**
+ * # Schema Methods
+ */
+slotSchema.methods.sign = function (callback) : void {
+  this.signed = true;
+  this.save(callback);
+};
 
 /**
  * # Model
