241e6c12730c86905bd8823c1332f13bdb301b38
Simon Knott <simoknott@gmail.com>
2018-04-07 14:36:35 +0200

    add mock router to satisfy ts :D

--- packages/ui/src/components/AuthenticatedRoute/index.spec.tsx
+++ packages/ui/src/components/AuthenticatedRoute/index.spec.tsx
@@ -2,11 +2,14 @@ import * as React from "react";
 import { AuthenticatedRoute } from "./index";
 import { shallow } from "enzyme";
 import * as renderer from "react-test-renderer";
+import { getMockRouterProps } from "../../../test_helpers/mockRouter";
 
 describe("AuthenticatedRoute", () => {
   const text = "hallo";
   const comp = shallow(
-    <AuthenticatedRoute isLoggedIn={true}>{text}</AuthenticatedRoute>
+    <AuthenticatedRoute {...getMockRouterProps<{}>({})} isLoggedIn={true}>
+      {text}
+    </AuthenticatedRoute>
   );
 
   it("renders correctly", () => {
@@ -21,7 +24,9 @@ describe("AuthenticatedRoute", () => {
 
   describe("when is not LoggedIn", () => {
     const notLoggedIn = shallow(
-      <AuthenticatedRoute isLoggedIn={false}>{text}</AuthenticatedRoute>
+      <AuthenticatedRoute {...getMockRouterProps<{}>({})} isLoggedIn={false}>
+        {text}
+      </AuthenticatedRoute>
     );
 
     it("returns a redirect", () => {
---
+++ packages/ui/test_helpers/mockRouter.ts
@@ -0,0 +1,38 @@
+import { RouteComponentProps } from "react-router";
+import { Location, UnregisterCallback } from "history";
+
+export function getMockRouterProps<P>(data?: P) {
+  const location: Location = {
+    hash: "",
+    key: "",
+    pathname: "",
+    search: "",
+    state: {}
+  };
+
+  const props: RouteComponentProps<P> = {
+    location,
+    match: {
+      isExact: true,
+      params: data,
+      path: "",
+      url: ""
+    },
+    history: {
+      location,
+      length: 2,
+      action: "POP",
+      push: () => {},
+      replace: () => {},
+      go: num => {},
+      goBack: () => {},
+      goForward: () => {},
+      block: t => null,
+      createHref: t => "",
+      listen: t => null
+    },
+    staticContext: {}
+  };
+
+  return props;
+}
