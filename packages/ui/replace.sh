af60543ff4ec35c0991e33de7be477ca8a550ec6
Simon Knott <simoknott@gmail.com>
2018-05-07 15:36:12 +0200

    Get config from cookie

--- packages/ui/replace.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sed -i 's/<p>{{config}}<\/p>/<script src="\/config.js"><\/script>/g' /app/index.html
--- packages/ui/src/typings.d.ts
+++ packages/ui/src/typings.d.ts
@@ -3,11 +3,3 @@ declare module "raven-for-redux";
 declare module "react-loadable";
 declare module "raven-js";
 declare module "react-router-enzyme-context";
-
-interface Config {
-  SENTRY_DSN_API?: string;
-}
-
-interface Window {
-  __env?: Config;
-}
