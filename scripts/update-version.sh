e1a38a0d4b18ad3c06dfde0702bbf4721d330a78
Simon Knott <simoknott@gmail.com>
2018-08-04 14:30:22 +0200

    0.2.3

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -6,6 +6,6 @@ lerna publish --yes --cd-version $version --skip-git
 
 lernaVersion=$(cat lerna.json | jq -r '.version')
 
-git commit -m "$lernaVersion"
+git commit -am "$lernaVersion"
 git tag "$lernaVersion"
 git push
