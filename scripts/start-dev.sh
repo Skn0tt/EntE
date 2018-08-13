2fe6e480f4b3b193f1ef4832959b57217dc7aaa2
Simon Knott <simoknott@gmail.com>
2018-08-13 09:07:15 +0200

    allow insecure in dev

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -3,6 +3,6 @@
 tempfile=$(mktemp)
 docker-app render --set API_VERSION=latest --set UI_VERSION=latest > $tempfile
 
-docker-compose -f $tempfile -f docker-compose.dev.yml up
+docker-compose -f "$tempfile" -f docker-compose.dev.yml up
 
-rm $tempfile
+rm "$tempfile"
