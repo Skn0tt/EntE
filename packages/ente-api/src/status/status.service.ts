7e256ea2a5d1775350ee42b8592e29e12c73e182
Simon Knott <simoknott@gmail.com>
2019-04-27 17:27:33 +0200

    implement db health check

--- packages/ente-api/src/status/status.service.ts
+++ packages/ente-api/src/status/status.service.ts
@@ -1,12 +1,14 @@
 import { Injectable, Inject } from "@nestjs/common";
 import { RedisService } from "../infrastructure/redis.service";
 import { SignerService } from "../infrastructure/signer.service";
+import { DbHealthIndicator } from "db/db-health-indicator";
 
 export interface HealthReport {
   isHealthy: boolean;
   dependencies: {
     redis: boolean;
     signer: boolean;
+    db: boolean;
   };
 }
 
@@ -14,21 +16,25 @@ export interface HealthReport {
 export class StatusService {
   constructor(
     @Inject(RedisService) private readonly redisService: RedisService,
-    @Inject(SignerService) private readonly signerService: SignerService
+    @Inject(SignerService) private readonly signerService: SignerService,
+    @Inject(DbHealthIndicator)
+    private readonly dbHealthIndicator: DbHealthIndicator
   ) {}
 
   async getStatus(): Promise<HealthReport> {
-    // TODO: DB check
+    // TODO: email check
 
     const redisIsHealthy = await this.redisService.isHealthy();
     const signerIsHealthy = await this.signerService.isHealthy();
+    const dbIsHealthy = await this.dbHealthIndicator.isHealthy();
 
-    const enteIsHealthy = redisIsHealthy && signerIsHealthy;
+    const enteIsHealthy = redisIsHealthy && signerIsHealthy && dbIsHealthy;
     const healthReport: HealthReport = {
       isHealthy: enteIsHealthy,
       dependencies: {
         signer: signerIsHealthy,
-        redis: redisIsHealthy
+        redis: redisIsHealthy,
+        db: dbIsHealthy
       }
     };
 
