612541285d97dbcc92dd00dc5b294004147c810a
Simon Knott <simoknott@gmail.com>
2019-09-08 23:00:02 +0200

    Move dockerapp-update into "version" npm script

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -6,8 +6,6 @@ lerna publish --force-publish --yes --cd-version $version --skip-git
 
 lernaVersion="v$(cat lerna.json | jq -r '.version')"
 
-./scripts/dockerapp-version.js update
-
 git commit -am "$lernaVersion"
 git tag "$lernaVersion"
 git push
