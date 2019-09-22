bb724426a26cf30ab702465ff01e284e1debad1c
Simon Knott <simoknott@gmail.com>
2019-09-22 15:14:18 +0200

    Add Test suite for SchiLD parser

--- packages/ente-ui/src/schild-import/schild.parser.spec.ts
+++ packages/ente-ui/src/schild-import/schild.parser.spec.ts
@@ -1,4 +1,10 @@
-import { sanitizeName, deriveUsername } from "./schild.parser";
+import {
+  sanitizeName,
+  deriveUsername,
+  parseWithValidation
+} from "./schild.parser";
+import * as _ from "lodash";
+import { Roles, CreateUserDto } from "ente-types";
 
 describe("sanitizeName", () => {
   test.each`
@@ -11,6 +17,60 @@ describe("sanitizeName", () => {
 });
 
 describe("deriveUsername", () => {
+  test.each`
+    first                      | last         | expected
+    ${"Marie Kahle"}           | ${"Kolbach"} | ${"marie_kahle.kolbach"}
+    ${"Marie Eva Lotta Kahle"} | ${"Kolbach"} | ${"marie_eva_lotta_kahle.kolbach"}
+  `(
+    "deriveUsername($first, $last) === $expected",
+    ({ first, last, expected }) => {
+      expect(deriveUsername(first, last)).toBe(expected);
+    }
+  );
+});
+
+describe("parseWithValidation", () => {
+  describe("when there are no existing students", () => {
+    const existingStudentUsernames: string[] = [];
+
+    describe("given students with two parents", () => {
+      const input = `
+"Vorname";"Nachname";"vorauss. Abschlussdatum";"Jahrgang";"Erzieher 1: Anrede";"Erzieher 1: Vorname";"Erzieher 1: Nachname";"Erzieher 2: Anrede";"Erzieher 2: Vorname";"Erzieher 2: Nachname";"Erzieher: E-Mail";"E-Mail (privat)";"Geburtsdatum"
+"Anton";"Krüger";31.07.2022;"EF";"Frau";"Anna";"Krüger";"Herr";"Thomas";"Krüger";"familie@krueger.de";"anton@krueger.de";01.01.2004
+      `.trim();
+
+      const result = parseWithValidation(input, existingStudentUsernames);
+
+      it("returns valid students", () => {
+        expect(result.isSuccess()).toBe(true);
+      });
+
+      it("returns students and one parent per student (not two, because there is only one email address", () => {
+        const { "anton.krüger": anton, krüger: krüger } = _.keyBy(
+          result.success(),
+          u => u.username
+        );
+
+        expect(anton).toEqual({
+          birthday: "2004-01-01",
+          username: "anton.krüger",
+          children: [],
+          displayname: "Anton Krüger",
+          email: "anton@krueger.de",
+          graduationYear: 2022,
+          role: Roles.STUDENT
+        } as CreateUserDto);
+        expect(krüger).toEqual({
+          username: "krüger",
+          children: ["anton.krüger"],
+          displayname: "Anna Krüger",
+          email: "familie@krueger.de",
+          role: Roles.PARENT
+        } as CreateUserDto);
+      });
+    });
+  });
+
   test.each`
     first                      | last         | expected
     ${"Marie Kahle"}           | ${"Kolbach"} | ${"marie_kahle.kolbach"}
