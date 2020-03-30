a00079f78ff34db5a216c464e6ff8f82264f0cca
Simon Knott <info@simonknott.de>
2020-03-30 16:18:12 +0200

    Rename out folder to "public"

--- docs/build.sh
+++ docs/build.sh
@@ -1,7 +1,7 @@
 (cd en && mkdocs build)
 (cd de && mkdocs build)
 
-mkdir build build/de build/en
-cp -R de/site/* build/
-cp -R en/site/* build/en/
-cp -R de/site/* build/de/
+mkdir public public/de public/en
+cp -R de/site/* public/
+cp -R en/site/* public/en/
+cp -R de/site/* public/de/
