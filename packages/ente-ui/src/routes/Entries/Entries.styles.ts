d78fa68efff2dc0b59f45067bddacb4f3c8077e3
Simon Knott <info@simonknott.de>
2020-04-04 13:36:11 +0200

    Remove unused mailAvatar styles

--- packages/ente-ui/src/routes/Entries/Entries.styles.ts
+++ packages/ente-ui/src/routes/Entries/Entries.styles.ts
@@ -23,12 +23,6 @@ const styles = (theme: Theme): StyleRules => ({
   },
   table: {
     overflowX: "auto"
-  },
-  mailAvatar: {
-    width: theme.spacing.unit,
-    height: theme.spacing.unit,
-    backgroundColor: theme.palette.primary.main,
-    color: theme.palette.primary.contrastText
   }
 });
 
