1109ce2d975c4d8accbe3fab559090144a793466
Simon Knott <info@simonknott.de>
2020-04-16 10:50:18 +0200

    Combine tsc and unit tests into "test" cmd

--- src/ui/components/LoginStatus.spec.tsx
+++ src/ui/components/LoginStatus.spec.tsx
@@ -9,23 +9,11 @@
 import * as React from "react";
 import { shallow } from "enzyme";
 import { LoginStatus } from "./LoginStatus";
-import { Some } from "monet";
 
 describe("LoginStatus", () => {
-  const logout = jest.fn();
-  const displayname = "Greg";
-
-  const comp = shallow(
-    <LoginStatus displayname={Some(displayname)} logout={logout} />
-  );
+  const comp = shallow(<LoginStatus />);
 
   it("renders correctly", () => {
     expect(comp).toMatchSnapshot();
-  });
-
-  it("logs out on clicking logout button", () => {
-    comp.find("#logout").simulate("click");
-
-    expect(logout).toBeCalled();
   });
 });
