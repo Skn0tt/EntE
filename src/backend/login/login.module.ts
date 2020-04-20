a0be59c3ea6ec9ad992e5c4f3c3cb0deacc1213f
Simon Knott <info@simonknott.de>
2020-04-20 13:27:43 +0200

    Module, not Service ...

--- src/backend/login/login.module.ts
+++ src/backend/login/login.module.ts
@@ -4,11 +4,11 @@ import { DbModule } from "../db/db.module";
 import { LoginService } from "./login.service";
 import { LoginController } from "./login.controller";
 import { ReviewedRecordsModule } from "../reviewedRecords/reviewedRecords.module";
-import { UsersService } from "../users/users.service";
+import { UsersModule } from "../users/users.module";
 
 @Module({
   controllers: [LoginController],
   providers: [LoginService],
-  imports: [TokenModule, DbModule, ReviewedRecordsModule, UsersService],
+  imports: [TokenModule, DbModule, ReviewedRecordsModule, UsersModule],
 })
 export class LoginModule {}
