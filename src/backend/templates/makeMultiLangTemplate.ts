db7593c116e5ccbcbab5347421bdd1069f65c22d
Simon Knott <info@simonknott.de>
2020-04-24 09:55:20 +0200

    Add 2FA notification templates

---
+++ src/backend/templates/makeMultiLangTemplate.ts
@@ -0,0 +1,19 @@
+import { Languages } from "@@types";
+import { mjml2html } from "../helpers/mjml";
+
+interface Template<T> {
+  title: string;
+  template: HandlebarsTemplateDelegate<T>;
+}
+
+type TemplatesByLanguage<T> = Record<Languages, Template<T>>;
+
+export function makeMultiLangTemplate<T>(templates: TemplatesByLanguage<T>) {
+  return function (lang: Languages, payload: T) {
+    const { template, title } = templates[lang];
+    const mjml = template(payload);
+    const { errors, html } = mjml2html(mjml);
+    if (errors.length > 0) throw new Error("MJML Error");
+    return { html, subject: title };
+  };
+}
