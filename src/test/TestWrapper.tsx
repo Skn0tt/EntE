bf1960c182200bc6a371b56e00bccc4a417b9382
Simon Knott <info@simonknott.de>
2020-11-27 16:41:28 +0100

    Fix tests

--- src/test/TestWrapper.tsx
+++ src/test/TestWrapper.tsx
@@ -2,8 +2,6 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import { initialState } from "ui/redux/reducer";
 import { useMemo } from "react";
-import { UserN } from "ui/redux";
-import { Roles } from "@@types";
 
 export function TestWrapper(props: React.PropsWithChildren<{}>) {
   const { children } = props;
